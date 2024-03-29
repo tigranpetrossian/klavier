4c1cf9958d89a22f8fd638dd000bdad44373b9e5
Tigran <tgpett@gmail.com>
2024-03-29 17:09:27 +0400

    Add simple build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'Klavier',
+      fileName: 'klavier',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+});
