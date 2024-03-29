a3f764ae884197eae2eeb6076035d214729377c7
Tigran <tgpett@gmail.com>
2024-03-30 01:02:03 +0400

    Add active state to Key

--- src/Key.tsx
+++ src/Key.tsx
@@ -1,6 +1,7 @@
 import styles from 'klavier.module.css';
 import React from 'react';
 import { midiToNote } from 'midi/midi.utils.ts';
+import { KeyColor } from 'types.ts';
 
 type Props = {
   midiNumber: number;
@@ -8,32 +9,37 @@ type Props = {
   active: boolean;
 };
 
-const classNames = {
-  black: styles.blackKey,
-  white: styles.whiteKey,
-};
-
 export const Key = (props: Props) => {
-  const { midiNumber, firstNoteMidiNumber } = props;
+  const { active, midiNumber, firstNoteMidiNumber } = props;
   const { keyColor } = midiToNote(midiNumber);
   const position = getKeyPosition(midiNumber, firstNoteMidiNumber);
 
   return (
     <div
-      className={classNames[keyColor]}
+      className={buildClassName(keyColor, active)}
       style={{ '--grid-column-start': position } as React.CSSProperties}
     />
   );
 };
+
+const classNames = {
+  black: styles.blackKey,
+  white: styles.whiteKey,
+};
+
+function buildClassName(color: KeyColor, active: boolean) {
+  const baseClass = classNames[color];
+  return active ? `${baseClass} active` : baseClass;
+}
 
 // The keyboard is laid out on a horizontal CSS grid with a 1px gap.
 // Position represents a starting column: `grid-column-start` in CSS terms.
 // White keys span over 12 columns each, making the octave length 84 columns total.
 // Black keys span over 8 columns each, and are overlaid on top white keys.
 // Key positions (starting columns) are calculated by hand to match a real piano keyboard as closely as possible.
+const KEY_POSITIONS = [1, 8, 13, 22, 25, 37, 44, 49, 57, 61, 70, 73];
+const OCTAVE_LENGTH_IN_COLUMNS = 84;
 function getAbsoluteKeyPosition(midiNumber: number) {
-  const KEY_POSITIONS = [1, 8, 13, 22, 25, 37, 44, 49, 57, 61, 70, 73];
-  const OCTAVE_LENGTH_IN_COLUMNS = 84;
   const { octave } = midiToNote(midiNumber);
   return octave * OCTAVE_LENGTH_IN_COLUMNS + KEY_POSITIONS[midiNumber % KEY_POSITIONS.length];
 }
