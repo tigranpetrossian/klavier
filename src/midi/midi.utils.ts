5c2fd231e6a00ada3fd517bb15bc891549037a50
Tigran <tgpett@gmail.com>
2024-03-29 22:37:43 +0400

    Add few midi constants and utilities

---
+++ src/midi/midi.utils.ts
@@ -0,0 +1,39 @@
+import { Note } from 'types.ts';
+import {
+  BLACK_KEY_MIDI_NUMBERS,
+  MAX_MIDI_NUMBER,
+  MIDI_NUMBER_C0,
+  MIN_MIDI_NUMBER,
+  OCTAVE_LENGTH,
+} from './midi.constants.ts';
+
+export function midiToNote(midiNumber: number): Note {
+  if (!isMidiNumber(midiNumber)) {
+    throw new Error(
+      `getMidiNoteAttributes expects a valid midi number: 0-127, received ${midiNumber}`
+    );
+  }
+
+  return {
+    midiNumber,
+    keyColor: isBlackKey(midiNumber) ? 'black' : 'white',
+    octave: Math.floor((midiNumber - MIDI_NUMBER_C0) / OCTAVE_LENGTH),
+  };
+}
+
+export function isMidiNumber(input: unknown): input is number {
+  return (
+    typeof input === 'number' &&
+    Number.isInteger(input) &&
+    input >= MIN_MIDI_NUMBER &&
+    input <= MAX_MIDI_NUMBER
+  );
+}
+
+export function isBlackKey(midiNumber: number) {
+  return isMidiNumber(midiNumber) && BLACK_KEY_MIDI_NUMBERS.includes(midiNumber);
+}
+
+export function isWhiteKey(midiNumber: number) {
+  return isMidiNumber(midiNumber) && !BLACK_KEY_MIDI_NUMBERS.includes(midiNumber);
+}
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,6 @@
 export type KeyColor = 'black' | 'white';
+export type Note = {
+  keyColor: KeyColor;
+  midiNumber: number;
+  octave: number;
+};
