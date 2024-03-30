58c44bbd606e4b336aad850bfe1675a3ab736326
Tigran <tgpett@gmail.com>
2024-03-30 11:27:06 +0400

    Add basic range validation

--- demo/App.tsx
+++ demo/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Klavier } from '../src';
 
 function App() {
-  return <Klavier />;
+  return <Klavier noteRange={[60, 108]} />;
 }
 
 export default App;
--- src/Klavier.tsx
+++ src/Klavier.tsx
@@ -1,6 +1,7 @@
 import classNames from 'klavier.module.css';
 import { Key } from 'Key.tsx';
 import { range } from 'utils.ts';
+import { isMidiNumber } from 'midi/midi.utils.ts';
 
 export interface KlavierProps {
   activeNotes?: Array<number>;
@@ -10,6 +11,7 @@ export interface KlavierProps {
 export const Klavier = (props: KlavierProps) => {
   const { activeNotes = [], noteRange = [21, 108] } = props;
   const [first, last] = noteRange;
+  validateRange(noteRange);
 
   return (
     <div className={classNames.klavier}>
@@ -24,3 +26,20 @@ export const Klavier = (props: KlavierProps) => {
     </div>
   );
 };
+
+const ERRORS = {
+  INVALID_MIDI_VALUES: 'Note range must be within valid MIDI numbers (0-127).',
+  INVALID_RANGE_ORDER: 'The last note must be greater than the first.',
+};
+
+function validateRange([first, last]: KlavierProps['noteRange']) {
+  const receivedRangeDisplay = `Received range: [${first}, ${last}]`;
+
+  if (!isMidiNumber(first) || !isMidiNumber(last)) {
+    throw new Error(`${ERRORS.INVALID_MIDI_VALUES} ${receivedRangeDisplay}`);
+  }
+
+  if (first >= last) {
+    throw new Error(`${ERRORS.INVALID_RANGE_ORDER} ${receivedRangeDisplay}`);
+  }
+}
