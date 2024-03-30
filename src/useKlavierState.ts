a3fa002fe2e2814372ca97d304d79b729e1aceb7
Tigran <tgpett@gmail.com>
2024-03-30 17:09:02 +0400

    Clear active notes when played for the first time

--- src/useKlavierState.ts
+++ src/useKlavierState.ts
@@ -2,6 +2,7 @@ import { useReducer } from 'react';
 
 type State = {
   activeNotes: Array<number>;
+  touched: boolean;
 };
 
 type Action =
@@ -15,20 +16,17 @@ type Action =
     };
 
 export function useKlavierState(initialActiveNotes: Array<number>) {
-  const [state, dispatch] = useReducer(reducer, { activeNotes: initialActiveNotes });
+  const [state, dispatch] = useReducer(reducer, {
+    activeNotes: initialActiveNotes,
+    touched: false,
+  });
 
   const playNote = (midiNumber: number) => {
-    dispatch({
-      type: 'NOTE_ON',
-      payload: midiNumber,
-    });
+    dispatch({ type: 'NOTE_ON', payload: midiNumber });
   };
 
   const stopNote = (midiNumber: number) => {
-    dispatch({
-      type: 'NOTE_OFF',
-      payload: midiNumber,
-    });
+    dispatch({ type: 'NOTE_OFF', payload: midiNumber });
   };
 
   return {
@@ -43,12 +41,20 @@ export function useKlavierState(initialActiveNotes: Array<number>) {
 function reducer(state: State, action: Action) {
   switch (action.type) {
     case 'NOTE_ON':
+      if (!state.touched) {
+        return {
+          ...state,
+          touched: true,
+          activeNotes: [action.payload],
+        };
+      }
       return {
         ...state,
         activeNotes: state.activeNotes.includes(action.payload)
           ? state.activeNotes
           : [...state.activeNotes, action.payload],
       };
+
     case 'NOTE_OFF':
       return {
         ...state,
