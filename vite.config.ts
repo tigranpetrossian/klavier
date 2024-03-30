319d06f7bca03f54a2a36993825bb24a7af7e9c3
Tigran <tgpett@gmail.com>
2024-03-30 21:50:50 +0400

    Update few packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import postCssNesting from 'postcss-nesting';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   build: {
