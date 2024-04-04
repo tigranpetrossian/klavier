6528affdb8a69946c5a76b7be6e2c00d8b2df58b
Tigran <tgpett@gmail.com>
2024-04-04 17:31:17 +0400

    Enable https in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import postCssNesting from 'postcss-nesting';
+import mkcert from 'vite-plugin-mkcert';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [mkcert(), tsconfigPaths(), react()],
+
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
